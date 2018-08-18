a0b4aafd7cf6d2a55d73df5a29c09c813602e8e4
miriamfrank23 <miriamfrank23@gmail.com>
2018-08-17 21:18:00 -0400

    bachelor methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = ""
+  array = []
+  data[season].each do |item|
+      if item["status"] == "Winner"
+      first_name = item["name"]
+    end
+  end
+array = first_name.split(" ")
+array[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season, contestants|
+      contestants.each do |contestant|
+        if contestant["hometown"] == hometown
+          return contestant["occupation"]
+        end
+      end
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  ages_num = []
+  data[season].each do |item|
+    ages.push(item["age"])
+  end
+  ages_num = ages.collect(&:to_i)
+  return (ages_num.reduce(:+) / ages_num.size.to_f).round(0)
 end
