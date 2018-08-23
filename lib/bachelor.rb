a1329e0a320d67011672915f13000fff3df04ef8
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 13:06:43 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  contestants = data[season]
+  contestants.each do |attributes|
+    if attributes["status"] == "Winner"
+      return attributes["name"].split(' ')[0]
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |attributes|
+      if attributes["occupation"] == occupation
+        return attributes["name"]
+      end 
+    end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |attributes|
+      if attributes["hometown"] == hometown 
+        count += 1 
+      end 
+    end 
+  end 
+  count 
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |attributes|
+      if attributes["hometown"] == hometown 
+        return attributes["occupation"]
+      end
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestants = data[season]
+  sum = 0
+  contestants.each do |attrs|
+    sum += attrs["age"].to_i
+  end
+  sum.to_f / contestants.length.to_f >= (sum / contestants.length) + 0.5 ? (sum / contestants.length) + 1 : sum / contestants.length 
 end
